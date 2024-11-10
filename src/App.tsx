c09d3fc2ec2506136bf6aade0a5d26a9da957afa
Cristian Contreras <crontreras1@gmail.com>
2024-11-09 21:30:31 -0500

    class 9. SPA & SSR

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { Link, Route, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">🏠</Link>
+
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={ < Home /> }></Route>
+
+        <Route path="/about" element={ < About /> }></Route>
+      </Routes>
+    </div>
+  )
+}
+
+function Home () {
+  return (
+    <h2>Home</h2>
+  )
+}
+
+function About () {
+  return (
+    <h2>About</h2>
+  )
+}
+
+export default App
